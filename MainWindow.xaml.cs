97f94dab2fb0a4c952840fb77e95ad009172e97a
Khalid <tkhalid@miami.edu>
2020-05-20 11:56:30 -0400

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,311 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PupConvertApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+       
+        // **************** Global Variables ******************
+        String Main_Input = "";
+        String Main_Output = "";
+        Double Conversion_result = 1.0;
+        bool decimalPoint = false;
+
+        readonly MetricRatio mymetric = new MetricRatio
+        {
+            converTo = length,
+            input = 1,
+            inputTypeIndex = 1,
+            outputTypeIndex = 2
+        };
+
+        //*****************************************************
+        public MainWindow()
+        {
+            InitializeComponent();
+                        
+            Type_Menu();
+
+            Init_Menu();
+            
+            Init_display();
+
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+            if (Input_validate())
+            {
+                Update_Display();
+            }
+            else
+            {
+               /* if (Input_TextBox.Text.Length >= 1)
+                {
+                    Input_TextBox.Text = Input_TextBox.Text.Substring(0,Input_TextBox.Text.Length - 1);
+                    Input_TextBox.SelectionStart = Input_TextBox.Text.Length;
+                    Input_TextBox.SelectionLength = 0;
+                    Console.WriteLine("Removed");
+                    Console.WriteLine(Input_TextBox.Text);
+                    Console.WriteLine(Input_TextBox.Text.Length);
+
+                }
+                */
+                
+            }
+            
+            
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("clear");
+            if (Output_TextBlock != null)
+            {
+                Init_display();
+
+            }
+        }
+
+
+        void Init_display()
+        {
+
+            if (Output_TextBlock != null || Input_TextBox != null)
+            {
+                Input_TextBox.BorderThickness = new Thickness(0.0);
+                Output_TextBlock.Text = "";
+                Input_TextBox.Text = "";
+                Input_TextBox.Focus();
+
+            }
+        }
+
+        void Update_Display()
+        {
+            Conversion_result = mymetric.Convert();
+            Decimal value = Convert.ToDecimal(Conversion_result);
+            String Result_String;
+            String Input_String;
+
+            if (Conversion_result > 1.0)    {
+                Result_String = value.ToString("N2"); 
+            }
+            else
+            {
+                Result_String = value.ToString("N8");
+            }
+
+            if (mymetric.input > 1.0)
+            {
+                Input_String = mymetric.input.ToString("N02");
+            }
+            else
+            {
+                Input_String = mymetric.input.ToString("N08");
+            }
+            
+            Main_Output = $"{Input_String}\n{mymetric.Input_type()}\nis\n{ Result_String }\n{mymetric.Output_type()}";
+            Console.WriteLine(Main_Output);
+           
+
+            if (Output_TextBlock != null)
+            {
+                if (Main_Input != "")
+                {
+                    Output_TextBlock.Text = Main_Output;
+                    Input_TextBox.Focus();
+                }
+                else
+                {
+                    Output_TextBlock.Text = "";
+                    Input_TextBox.Focus();
+                }
+
+            }
+        }
+
+        private void Input_select_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Console.WriteLine(Input_select.SelectedIndex);
+            mymetric.inputTypeIndex = Input_select.SelectedIndex + 1;
+            Update_Display();
+        }
+
+        private void Output_select_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Console.WriteLine(Output_select.SelectedIndex);
+            mymetric.outputTypeIndex = Output_select.SelectedIndex + 1;
+            Update_Display();
+        }
+
+        void Init_Menu()
+        {
+            if (mymetric.converTo != null)
+            {
+                Input_select.Items.Clear();
+                Output_select.Items.Clear();
+
+                Console.WriteLine(mymetric.converTo.GetUpperBound(0));
+                
+                foreach (var item in mymetric.Menu_items())
+                {
+                    Console.WriteLine(item);
+                    Input_select.Items.Add(item);
+                    Output_select.Items.Add(item);
+                }
+
+                Input_select.SelectedIndex = 1;
+                Output_select.SelectedIndex = 2;
+            }
+
+        }
+
+        void Type_Menu()
+        {
+            if (mymetric.converTo != null)
+            {
+                Type_Select.Items.Clear();
+                
+                Console.WriteLine(MainMenuType.Count);
+               
+                foreach (var item in MainMenuType)
+                {
+                    Console.WriteLine(item);
+                    Type_Select.Items.Add(item);
+                    
+                }
+
+                Type_Select.SelectedIndex = 1;
+                                
+            }
+
+            Update_Display();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+           switch (Type_Select.SelectedIndex)
+            {
+                case 0: mymetric.converTo = dataStorage; break;
+
+                case 1: mymetric.converTo = length; break;
+
+                case 2: mymetric.converTo = speed; break;
+
+                case 3: mymetric.converTo = volume; break;
+            }
+
+            Init_Menu();
+            Init_display();
+
+        }
+
+        private bool Input_validate()
+        {
+            Main_Input = Input_TextBox.Text;
+
+            if (Main_Input.Length<1)
+            {
+                Input_TextBox.Text = "";
+                return false;
+            }
+
+            if (Main_Input.Length >= 16)
+            {
+                Input_TextBox.Text = Input_TextBox.Text.Substring(0, Input_TextBox.Text.Length - 1);
+                Input_TextBox.SelectionStart = Input_TextBox.Text.Length;
+                Input_TextBox.SelectionLength = 0;
+                Console.WriteLine("Removed");
+                Console.WriteLine(Input_TextBox.Text);
+                Console.WriteLine(Input_TextBox.Text.Length);
+                return false;
+            }
+
+            string ch = Main_Input.Substring(Main_Input.Length - 1);
+            Console.WriteLine(ch);
+
+            switch (ch)
+            {
+                case "0": break;
+                case "1": break;
+                case "2": break;
+                case "3": break;
+                case "4": break;
+                case "5": break;
+                case "6": break;
+                case "7": break;
+                case "8": break;
+                case "9": break;
+                case ".": break;
+
+                default:
+                    {
+                        if (Input_TextBox.Text.Length >= 1)
+                        {
+                            Input_TextBox.Text = Input_TextBox.Text.Substring(0, Input_TextBox.Text.Length - 1);
+                            Input_TextBox.SelectionStart = Input_TextBox.Text.Length;
+                            Input_TextBox.SelectionLength = 0;
+                            Console.WriteLine("Removed");
+                            Console.WriteLine(Input_TextBox.Text);
+                            Console.WriteLine(Input_TextBox.Text.Length);
+
+                        }
+                        return false;
+                    }
+                    
+            }
+            // end check non-numeric
+
+                    
+            if (double.TryParse(Main_Input, out double doubleValue))
+            {
+                // Here you already can use a valid double 'doubleValue'
+                mymetric.input = doubleValue;
+            }
+            else
+            {
+                // Here you can display an error message like 'Invalid value'
+                Console.WriteLine("Invalid Input");
+                if (Input_TextBox.Text.Length >= 1)
+                {
+                    Input_TextBox.Text = Input_TextBox.Text.Substring(0, Input_TextBox.Text.Length - 1);
+                    Input_TextBox.SelectionStart = Input_TextBox.Text.Length;
+                    Input_TextBox.SelectionLength = 0;
+                    Console.WriteLine("Removed");
+                    Console.WriteLine(Input_TextBox.Text);
+                    Console.WriteLine(Input_TextBox.Text.Length);
+
+                }
+                //Main_Input = "";
+            }
+
+            return true;
+        }
+    }
+
+   
+
+}
---
+++ MetricRatio.cs
@@ -0,0 +1,124 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PupConvertApp
+{
+    public partial class MainWindow : Window
+    {
+        public struct DataType
+        {
+            public string type;
+            public double value;
+
+            public DataType(string type, double value)
+            {
+                this.type = type;
+                this.value = value;
+            }
+        }
+
+        static DataType byte_ = new DataType(type: "byte(s)", value: 1.0);
+        static DataType kiloByte = new DataType(type: "Kilo Byte(s)", value: 1024.0);
+        static DataType megaByte = new DataType(type: "Mega Byte(s)", value: 1024.0 * 1024.0);
+        static DataType gigaByte = new DataType(type: "Giga Byte(s)", value: 1024.0 * 1024.0 * 1024.0);
+        static DataType teraByte = new DataType(type: "Tera Byte(s)", value: 1024.0 * 1024.0 * 1024.0 * 1024.0);
+        static DataType petabyte = new DataType(type: "Peta Byte(s)", value: 1024.0 * 1024.0 * 1024.0 * 1024.0 * 1024.0);
+
+        static DataType mm = new DataType(type: "Millimeter(s)", value: 0.001);
+        static DataType cm = new DataType(type: "Centimeter(s)", value: 0.01);
+        static DataType m = new DataType(type: "Meter(s)", value: 1.0);
+        static DataType km = new DataType(type: "Kilometer(s)", value: 1000.0);
+        static DataType foot = new DataType(type: "Foot(s)", value: 0.3048);
+        static DataType yard = new DataType(type: "Yard(s)", value: 0.9144);
+        static DataType mile = new DataType(type: "Mile(s)", value: 1609.344);
+
+        static DataType MpH = new DataType(type: "Mile(s) per Hour", value: 0.447);
+        static DataType KmpH = new DataType(type: "Kilometer(s) per Hour", value: 0.2778);
+        static DataType Fps = new DataType(type: "Feet per second", value: 0.3048);
+        static DataType mps = new DataType(type: "Meter(s) per second", value: 1.0);
+        static DataType knot = new DataType(type: "knot(s)", value: 0.5144);
+
+        static DataType liter  = new DataType(type: "Liter(s)", value: 1.0);
+        static DataType gallon = new DataType(type: "Gallon(s)", value: 0.264172);
+        static DataType quart  = new DataType(type: "Quart(s)", value: 1.05669);
+        static DataType pint   = new DataType(type: "Pint", value: 2.11338);
+        static DataType cup    = new DataType(type: "Cup(s)", value: 4.16667);
+        static DataType ounce   = new DataType(type: "Ounce(s)", value: 33.814);
+        static DataType cubicMeter  = new DataType(type: "Cubic Meter(s)", value: 0.001);
+        static DataType milliLiter = new DataType(type: "Milli Liter(s)", value: 1000);
+        static DataType cubicFoot = new DataType(type: "Cubic Foot", value: 0.0353147);
+        static DataType cubicInch = new DataType(type: "Cubic Inch", value: 61.0237);
+
+
+        static DataType dummyVal = new DataType(type: "", value: 1.0);
+        public static DataType[] initVal = { dummyVal };
+        public static DataType[] dataStorage = { dummyVal, byte_, kiloByte, megaByte, gigaByte, teraByte, petabyte };
+        public static DataType[] length = { dummyVal, mm, cm, m, km, foot, yard, mile };
+        public static DataType[] speed = { dummyVal, MpH, KmpH, Fps, mps, knot };
+        public static DataType[] volume = { dummyVal, liter, gallon, quart, pint, cup, ounce, cubicMeter, milliLiter, cubicFoot, cubicInch };
+
+
+        public static List<string> MainMenuType = new List<string>(){ "Datastorage", "Length", "Speed", "Volume" };
+
+
+        public class MetricRatio
+        {
+            public double input = 1;
+            public int inputTypeIndex = 1;
+            public int outputTypeIndex = 1;
+            public DataType[] converTo = initVal;
+
+            /* public MetricRatio()
+             {
+                 this.input = 1;
+                 this.inputType = 1;
+                 this.outputType = 1;
+                 this.converTo = initVal;
+             } */
+
+            public double Convert()
+            {
+                return input * (converTo[inputTypeIndex].value / converTo[outputTypeIndex].value);
+            }
+
+            public string Input_type()
+            {
+                return converTo[inputTypeIndex].type;
+            }
+
+            public string Output_type()
+            {
+                return converTo[outputTypeIndex].type;
+            }
+
+            public List<string> Menu_items()
+            {
+                List<string> arr = new List<string>();
+
+
+                for (int i = 1; i <= converTo.GetUpperBound(0); i++)
+                {
+                    arr.Add(converTo[i].type);
+                }
+
+
+                return arr;
+
+            }
+                       
+        }
+    }
+}
